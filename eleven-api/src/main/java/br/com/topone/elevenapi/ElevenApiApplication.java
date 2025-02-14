fb09fb7c01e0748bb4f39543ceb2d13048a17167
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2025-02-14 14:05:10 -0300

    Created Project

---
+++ eleven-api/src/main/java/br/com/topone/elevenapi/ElevenApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.topone.elevenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevenApiApplication.class, args);
+    }
+
+}
---
+++ eleven-api/src/test/java/br/com/topone/elevenapi/ElevenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.topone.elevenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
