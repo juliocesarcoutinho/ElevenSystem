f33c6ced09b3b5f4b2281d97909fdb7455363b1e
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2025-03-11 08:57:49 -0300

    continuity

--- eleven-api/src/main/resources/import.sql
+++ eleven-api/src/main/resources/import.sql
@@ -1,7 +1,7 @@
 -- Inserir dados na tabela tb_user
-INSERT INTO tb_user (name, email, password, active, created_at, updated_at) VALUES ('Alex Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, NOW(), NOW());
-INSERT INTO tb_user (name, email, password, active, created_at, updated_at) VALUES ('Maria Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, NOW(), NOW());
-INSERT INTO tb_user (name, email, password, active, created_at, updated_at) VALUES ('Julio Coutinho', 'julio@gemmap.com.br', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, NOW(), NOW());
+INSERT INTO tb_user (name, email, password, active, created_at, updated_at) VALUES ('Rebekah Olivia', 'rebekah@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, NOW(), NOW());
+INSERT INTO tb_user (name, email, password, active, created_at, updated_at) VALUES ('Miriã Aquino', 'miria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, NOW(), NOW());
+INSERT INTO tb_user (name, email, password, active, created_at, updated_at) VALUES ('Julio Coutinho', 'julio@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG', 1, NOW(), NOW());
 
 -- Inserir dados na tabela tb_role
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
