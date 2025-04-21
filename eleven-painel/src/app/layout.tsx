82780a4ce4ee93d56f83c5ffe7aacf8f440536b6
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2025-04-21 00:21:55 -0300

    Revert "melhoria na responsividade"
    This reverts commit 39be7f26c69503842d84de0b8f39778e1ab4d8e3.
    
    # Conflicts:
    #	eleven-painel/src/components/users/UserForm.tsx
    #	eleven-painel/src/styles/components/forms.styles.ts

--- eleven-painel/src/app/layout.tsx
+++ eleven-painel/src/app/layout.tsx
@@ -1,38 +1,34 @@
-import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-    title: "Eleven Juventude",
-    description: "Plataforma de gerenciamento de atividades e events da Eleven Juventude",
-
+  title: "Eleven Juventude",
+  description: "Plataforma de gerenciamento de atividades e events da Eleven Juventude",
 };
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="pt-br">
-        <head>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        </head>
-        <body
-            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {children}
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
